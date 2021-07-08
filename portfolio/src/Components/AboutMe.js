45295d2c5a7ceb9544947b820961b8de50bfe458
shellshellies <michele.gao.shellies@icloud.com>
2021-07-07 21:13:41 -0400

    added photo for about me

--- portfolio/src/Components/AboutMe.js
+++ portfolio/src/Components/AboutMe.js
@@ -1,7 +1,10 @@
 export default function AboutMe(){
     return(
         <div>
-            hello from about me 
+            <img className="photo"src="https://media-exp3.licdn.com/dms/image/C4D03AQEbDBYTMsyPTw/profile-displayphoto-shrink_400_400/0/1621974221732?e=1631145600&v=beta&t=vwy6x948hllBL_BSc1FB_nVSWEKiqiGvu8YbsizOuZs"/>
+            <section>
+                Currently living in Brooklyn, New York. A mother, a coder, and live for the ability to be creative. 
+            </section>
         </div>
     )
 }

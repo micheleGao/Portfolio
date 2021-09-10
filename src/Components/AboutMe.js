68cab8da5c9e8c64bee4e4b03cb9f1173c7ce8be
shellshellies <michele.gao.shellies@icloud.com>
2021-09-10 15:21:32 -0400

    added photo

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -4,7 +4,7 @@ export default function AboutMe(){
     return(
         <div>
             
-            <img className="photo"src="https://media-exp3.licdn.com/dms/image/C4D03AQEbDBYTMsyPTw/profile-displayphoto-shrink_400_400/0/1621974221732?e=1631145600&v=beta&t=vwy6x948hllBL_BSc1FB_nVSWEKiqiGvu8YbsizOuZs"/>
+            <img className="photo" src="https://media-exp1.licdn.com/dms/image/C4D03AQEbDBYTMsyPTw/profile-displayphoto-shrink_100_100/0/1621974221732?e=1636588800&v=beta&t=UFuCsY0on7GZjqBHpOKWY86KGfLu171ugtKr0_XdMbs"/>
                 <div>
                     <h1 className="name">Michele Gao</h1>
                     <h2 className="title">Full Stack Software Engineer</h2>

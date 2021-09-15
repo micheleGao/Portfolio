b5c972ad05b88cbf722d9386275e84e2b2c362bf
shellshellies <michele.gao.shellies@icloud.com>
2021-09-15 14:07:36 -0400

    added photo asset for the about me page

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -1,10 +1,10 @@
 import { Card } from "react-bootstrap";
-
+import me from '../me.jpg';
 export default function AboutMe(){
     return(
         <div>
             
-            <img className="photo" src="https://media-exp1.licdn.com/dms/image/C4D03AQEbDBYTMsyPTw/profile-displayphoto-shrink_100_100/0/1621974221732?e=1636588800&v=beta&t=UFuCsY0on7GZjqBHpOKWY86KGfLu171ugtKr0_XdMbs"/>
+            <img className="photo" src={me}/>
                 <div>
                     <h1 className="name">Michele Gao</h1>
                     <h2 className="title">Full Stack Software Engineer</h2>
